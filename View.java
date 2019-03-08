a6b531bbb6364dc0cddc488e8bfaf855669b0c9d
Sean Nemes <snemes@udel.edu>
2019-03-08 16:37:02 -0500

    View constructor and imports

--- View.java
+++ View.java
@@ -7,6 +7,9 @@
  * use proper images for direction
  * load images for all direction (an image should only be loaded once!!! why?)
  **/
+import java.awt.Color;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class View extends JPanel {
 
@@ -16,14 +19,17 @@ public class View extends JPanel {
   private final int imgHeight = 165;
 
   public View() {
-    
+    JFrame frame = new JFrame();
+    frame.getContentPane().add(this);
+    frame.setBackground(Color.gray);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setSize(width, height);
+    frame.setVisible(true);
   }
 
   public void update(int modelX, int modelY, int modelDir) {
 
   }
-
-
 
   public int getWidth() {
     return width;
